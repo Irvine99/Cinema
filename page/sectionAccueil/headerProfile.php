48851ad0e2b5cd69e054fe5c28f76a1692435df3
Irvine99 <60505161+Irvine99@users.noreply.github.com>
2023-03-15 11:17:42 +0100

    acceuil a faire modif dans les sliders

---
+++ page/sectionAccueil/headerProfile.php
@@ -0,0 +1,8 @@
+<div class="py-3 flex justify-between">
+    <img class="h-[60px] w-auto" src="./../assets/img/Logo.png" alt="">
+    <div class="flex j items-end">
+        <h1 class="text-sm text-white mr-1">Bonjour</h1>
+        <h1 class="text-sm text-white mr-3">Stanislas</h1>
+        <img class="mb-[5px] h-[50px] w-[50px] rounded-full" src="./../assets/img/henrycavil.jpg" alt="" class=""> 
+    </div>
+</div>
