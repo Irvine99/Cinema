5663e03eef4f7f3e177aacd01769d1fc9ad75d8d
Irvine99 <60505161+Irvine99@users.noreply.github.com>
2023-03-16 14:51:29 +0100

    slider responsive

--- page/sectionAccueil/sliderOne.php
+++ page/sectionAccueil/sliderOne.php
@@ -1,4 +1,5 @@
-<!--Slider-->
+<div class="md:hidden">
+  <!--Slider-->
 
 
 <swiper-container class="mySwiper"  slides-per-view="1.5"
@@ -14,3 +15,46 @@
 
 
   <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-element-bundle.min.js"></script>
+</div>
+
+<!--Tablette-->
+
+<div class="hidden md:block lg:hidden">
+  <!--Slider-->
+
+
+<swiper-container class="mySwiper"  slides-per-view="3"
+    space-between="20" free-mode="true">
+    <swiper-slide><img class="w-auto h-[250px]" src="./../assets/img/pikadetective.jpg" alt="" class=""></swiper-slide>
+    <swiper-slide><img class="w-auto h-[250px]" src="./../assets/img/pikadetective.jpg" alt="" class=""></swiper-slide>
+    <swiper-slide><img class="w-auto h-[250px]" src="./../assets/img/pikadetective.jpg" alt="" class=""></swiper-slide>
+    <swiper-slide><img class="w-auto h-[250px]" src="./../assets/img/pikadetective.jpg" alt="" class=""></swiper-slide>
+    <swiper-slide><img class="w-auto h-[250px]" src="./../assets/img/pikadetective.jpg" alt="" class=""></swiper-slide>
+    <swiper-slide><img class="w-auto h-[250px]" src="./../assets/img/pikadetective.jpg" alt="" class=""></swiper-slide>
+
+    </swiper-container>
+
+
+  <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-element-bundle.min.js"></script>
+</div>
+
+<!--Desktop-->
+
+<div class="hidden lg:block">
+  <!--Slider-->
+
+
+<swiper-container class="mySwiper"  slides-per-view="5"
+    space-between="20" free-mode="true">
+    <swiper-slide><img class="w-auto h-[250px]" src="./../assets/img/pikadetective.jpg" alt="" class=""></swiper-slide>
+    <swiper-slide><img class="w-auto h-[250px]" src="./../assets/img/pikadetective.jpg" alt="" class=""></swiper-slide>
+    <swiper-slide><img class="w-auto h-[250px]" src="./../assets/img/pikadetective.jpg" alt="" class=""></swiper-slide>
+    <swiper-slide><img class="w-auto h-[250px]" src="./../assets/img/pikadetective.jpg" alt="" class=""></swiper-slide>
+    <swiper-slide><img class="w-auto h-[250px]" src="./../assets/img/pikadetective.jpg" alt="" class=""></swiper-slide>
+    <swiper-slide><img class="w-auto h-[250px]" src="./../assets/img/pikadetective.jpg" alt="" class=""></swiper-slide>
+
+    </swiper-container>
+
+
+  <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-element-bundle.min.js"></script>
+</div>
--- page/sectionAccueil/sliderTwo.php
+++ page/sectionAccueil/sliderTwo.php
@@ -1,6 +1,7 @@
 <h3 class="text-white my-3 text-1xl">Film populaire</h3>
 
-<!--Slider-->
+<div class="md:hidden">
+  <!--Slider-->
 
 
 <swiper-container class="mySwiper"  slides-per-view="1.5"
@@ -16,4 +17,47 @@
 
 
   <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-element-bundle.min.js"></script>
+</div>
+
+<!--Tablette-->
+
+<div class="hidden md:block lg:hidden">
+  <!--Slider-->
+
+
+<swiper-container class="mySwiper"  slides-per-view="3"
+    space-between="20" free-mode="true">
+    <swiper-slide><img class="w-auto h-[250px]" src="./../assets/img/pikadetective.jpg" alt="" class=""></swiper-slide>
+    <swiper-slide><img class="w-auto h-[250px]" src="./../assets/img/pikadetective.jpg" alt="" class=""></swiper-slide>
+    <swiper-slide><img class="w-auto h-[250px]" src="./../assets/img/pikadetective.jpg" alt="" class=""></swiper-slide>
+    <swiper-slide><img class="w-auto h-[250px]" src="./../assets/img/pikadetective.jpg" alt="" class=""></swiper-slide>
+    <swiper-slide><img class="w-auto h-[250px]" src="./../assets/img/pikadetective.jpg" alt="" class=""></swiper-slide>
+    <swiper-slide><img class="w-auto h-[250px]" src="./../assets/img/pikadetective.jpg" alt="" class=""></swiper-slide>
+
+    </swiper-container>
+
+
+  <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-element-bundle.min.js"></script>
+</div>
+
+<!--Desktop-->
+
+<div class="hidden lg:block">
+  <!--Slider-->
+
+
+<swiper-container class="mySwiper"  slides-per-view="5"
+    space-between="20" free-mode="true">
+    <swiper-slide><img class="w-auto h-[250px]" src="./../assets/img/pikadetective.jpg" alt="" class=""></swiper-slide>
+    <swiper-slide><img class="w-auto h-[250px]" src="./../assets/img/pikadetective.jpg" alt="" class=""></swiper-slide>
+    <swiper-slide><img class="w-auto h-[250px]" src="./../assets/img/pikadetective.jpg" alt="" class=""></swiper-slide>
+    <swiper-slide><img class="w-auto h-[250px]" src="./../assets/img/pikadetective.jpg" alt="" class=""></swiper-slide>
+    <swiper-slide><img class="w-auto h-[250px]" src="./../assets/img/pikadetective.jpg" alt="" class=""></swiper-slide>
+    <swiper-slide><img class="w-auto h-[250px]" src="./../assets/img/pikadetective.jpg" alt="" class=""></swiper-slide>
+
+    </swiper-container>
+
+
+  <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-element-bundle.min.js"></script>
+</div>
 
