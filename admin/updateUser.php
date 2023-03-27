5565c9ebdfb210b73e5ef58a20646fb50019593f
Irvine99 <60505161+Irvine99@users.noreply.github.com>
2023-03-27 11:16:37 +0200

    admin + crud user

---
+++ admin/updateUser.php
@@ -0,0 +1,82 @@
+<?php session_start() ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./../assets/css/sliderOne.css">
+    <link rel="stylesheet" href="./../assets/css/main.css">
+
+        <!--Tailwind-->
+        <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+    tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            gray: '#5E5E5E',
+            black: '#1C1A29',
+            red: '#E82626',
+            lightBlue: '#38354B',
+          }
+        }
+      }
+    }
+    </script>
+
+    <!--Flowbite-->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.4/flowbite.min.css"  rel="stylesheet" />
+    <!--Swiper-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css"/>
+
+    <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
+
+    <title>Acceuil</title>
+</head>
+<body class="h-screen bg-[url('./../assets/img/wallpaper.jpg')]">
+    <div class="h-full bg-black/70">
+     <div class="">
+        <a href="./../page/accueil.php"><img src="./../assets/img/back.png" alt="" class="pt-3 pl-3"></a>
+     </div>   
+     <div class="py-5 flex flex-col justify-center items-center">
+        <img class="rounded-full h-[150px] w-[150px]" src="./../assets/img/henrycavil.jpg" alt="">
+        <div class="flex gap-2 pt-1 ">
+            <h3 class="text-xl"><?= $_SESSION['username'] ?></h3>
+            <h3 class="text-xl"><?= $_SESSION['name'] ?></h3>
+        </div>
+     </div>
+     <div class="flex flex-col justify-center items-center">
+        <div class="bg-[#1C1A29]/80 p-1 rounded-lg md:w-1/2 lg:w-1/3 w-3/4">
+            <h1 class="text-xl mb-7 font-bold py-2 text-center">Modification de votre compte</h1>
+            <div class="mx-2 flex flex-col gap-1">
+                <h3 class="text-white/80">Modifier votre Prénom</h3>
+                <h3 class="text-white/80">Modifier votre Nom</h3>
+            </div>
+            <h1 class="mt-7 mb-2 text-xl mb-7 font-bold py-2 text-center">Information de votre compte</h1>
+            <div class="mx-2 flex gap-1">
+                <h3 class="text-white/80">Votre email : </h3>
+                <h3 class="text-white/80">Bougou@bougou.com </h3>
+                
+            </div>
+            <h1 class="mt-7 mb-2 text-xl mb-7 font-bold py-2 text-center">Suppression de votre compte</h1>
+            <div class="mb-3 flex justify-start mx-2 flex gap-1">
+                <a class ="bg-[#E82626] text-white py-1 px-4 rounded-lg" href="http://">Supprimer</a>
+                
+            </div>
+            <h1 class="mt-7 mb-2 text-xl mb-7 font-bold py-2 text-center">Vous deconnecter</h1>
+            <div class="mb-3 flex justify-start mx-2 flex gap-1">
+            <a  class ="bg-[#E82626] text-white py-1 px-4 rounded-lg" href="../traitement/logOut.php">Deconnection</a>
+            </div>
+            
+            
+        </div>    
+     </div>     
+    </div>
+
+</body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.4/flowbite.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+<script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
+</html>
--- admin/userPage.php
+++ admin/userPage.php
@@ -1,4 +1,5 @@
 <?php session_start() ?>
+<?php require('../traitement/function.php') ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -36,25 +37,58 @@
     <title>Acceuil</title>
 </head>
 <body class="h-screen bg-[url('./../assets/img/wallpaper.jpg')]">
-    <div class="h-full bg-black/70">
+    <div class="min-h-full bg-black/70">
      <div class="">
         <a href="./../page/accueil.php"><img src="./../assets/img/back.png" alt="" class="pt-3 pl-3"></a>
      </div>   
      <div class="py-5 flex flex-col justify-center items-center">
         <img class="rounded-full h-[150px] w-[150px]" src="./../assets/img/henrycavil.jpg" alt="">
         <div class="flex gap-2 pt-1 ">
-            <h3 class="text-xl"><?= $_SESSION['username'] ?></h3>
-            <h3 class="text-xl"><?= $_SESSION['name'] ?></h3>
+            <h3 class="text-xl"></h3>
+            <h3 class="text-xl"></h3>
         </div>
      </div>
-     <div class="flex flex-col justify-center items-center">
-        <div class="bg-[#1C1A29]/80 p-1 rounded-lg md:w-1/2 lg:w-1/3 w-3/4">
+
+        <?php
+        $users = userAll();
+        foreach ($users as $user) {
+            $id = $user['Id_users'];
+            $username = $user['username'];
+            $name = $user['name'];
+            $timestamp = $user['date_create'];
+            $date = DateTime::createFromFormat('Y-m-d H:i:s', $timestamp);
             
+        ?>
+        <div class="flex flex-col justify-center items-center">
+            <div class="bg-[#1C1A29]/80 p-1 rounded-lg md:w-2/3 lg:w-2/3 w-3/4">    
+                <div class="bg-[#5E5E5E]/80 p-2 ">
+                    <div class="flex md:flex-row flex-col justify-between gap-4 ">
+                        <div class="flex justify-start ">
+                            <h3 class=""><?= $username ?></h3>
+                            <h3 class="mx-2"><?= $name ?></h3>
+                        </div>
+                        <div class=" w-full flex justify-start">
+                          <h3 class="text-center text-sm">inscrit depuis le : <?= $date->format('d/m/Y') ?></h3>
+                        </div>
+                        <div class="flex justify-center md:justify-end  w-full gap-4">
+                            <form action="/updateUser.php" method="get">
+                            <button class="bg-blue-500 p-1 rounded-lg " type="submit">Modifier</button>
+                            </form>
+                            <form action="" method="get">
+                                <input type="hidden" value="<?= $id ?>" name="">
+                             <button class="bg-red-500 p-1 rounded-lg " type="submit">Supprimer</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>    
+        </div>        
+            <?php }
+            ?>
 
             
             
-        </div>    
-     </div>     
+  
     </div>
 
 </body>
